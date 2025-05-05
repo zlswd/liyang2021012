e5b4354b913882158308213d83102bbd92a51892
LeeAndMonkey <1798304528@qq.com>
2025-05-05 10:22:46 +0800

    修改express.js文件
    修改了部分代码，删除了一行注释

--- express.js
+++ express.js
@@ -14,9 +14,7 @@ app.post('/',(req,res) => {
     res.send('<h1>你好，Hello World!</h1>');
 });
 
-//监听3000端口
-app.listen(3000,() => {
+//监听6666端口
+app.listen(6666,() => {
     console.log('服务器已启动...');
 });
-
-//添加一行注释

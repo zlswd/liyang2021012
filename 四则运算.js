df9e3c40a30c6bb244b49724dbb48b596d502d0b
zlswd <2129652685@qq.com>
2025-05-19 11:20:59 +0800

    Add files via upload

---
+++ 四则运算.js
@@ -0,0 +1,19 @@
+//实现加法运算
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+//实现减法运算
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+
+//实现乘法运算
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+
+//实现除法运算fllename     diM
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
